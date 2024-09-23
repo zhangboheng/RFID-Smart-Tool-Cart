6e2bbe97e11909f08ddea99ea73b624aeadd0bcf
zhangboheng <zhangboheng827@gmail.com>
2024-09-23 11:19:19 +0800

    Import Packages

--- app/src/main/java/com/example/keyadministrator/CategoryControlActivity.java
+++ app/src/main/java/com/example/keyadministrator/CategoryControlActivity.java
@@ -25,6 +25,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
--- app/src/main/java/com/example/keyadministrator/UserControl.java
+++ app/src/main/java/com/example/keyadministrator/UserControl.java
@@ -26,6 +26,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
